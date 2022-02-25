3dcb6199cd8904d0f1fcd01c2ecd73e0e2c977ae
maribelniok <mariannabelniok@outlook.com>
2022-02-25 07:18:41 -0300

    navigation working

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -86,7 +86,7 @@ export default function Dashboard() {
                             height={160}
                         />
                     </Box>
-                    <Box p="8" bg="gray.800" borderRadius={8} pb="4">
+                    <Box p={["6", "8"]} bg="gray.800" borderRadius={8} pb="4">
                         <Text fontSize="lg" mb="4">
                             Taxa de abertura
                         </Text>
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,77 +1,101 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Checkbox,
+  Flex,
+  Heading,
+  Icon,
+  Table,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+  Text,
+  useBreakpointValue,
+} from "@chakra-ui/react";
+import Link from "next/link";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
-    return (
-        <Box>
-            <Header />
-            <Flex 
-                w="100%" 
-                my="6"
-                maxWidth={1480}
-                mx="auto"
-                px="6"
-            >
-                <Sidebar />
-                <Box flex="1" borderRadius={8} bg="gray.800" p="8">
-                    <Flex mb="8" justify="space-between" align="center">
-                        <Heading size="lg" fontWeight="normal"> Usuários </Heading>
-                        <Button 
-                            as="a" 
-                            size="sm" 
-                            fontSize="sm" 
-                            colorScheme="pink" 
-                            leftIcon={<Icon as={RiAddLine} fontSize="20" />}
-                        >
-                            Criar novo
-                        </Button>
-                    </Flex>
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  });
 
-                    <Table colorScheme="whiteAlpha">
-                        <Thead>
-                            <Tr>
-                                <Th px="6" color="gray.300" width="8">
-                                    <Checkbox colorScheme="pink" />
-                                </Th>
-                                <Th>Usuário</Th>
-                                <Th>Data de cadastro</Th>
-                                <Th w="8"></Th>
-                            </Tr>
-                        </Thead>
-                        <Tbody>
-                            <Tr px="6">
-                                <Td>
-                                    <Checkbox colorScheme="pink" />
-                                </Td>
-                                <Td>
-                                    <Box>
-                                        <Text fontWeight="bold">Marianna Belniok</Text>
-                                        <Text fontSize="sm" color="gray.300">mariannabelniok@outlook.com</Text>
-                                    </Box>
-                                </Td>
-                                <Td>
-                                    04 de Abril, 2021
-                                </Td>
-                                <Td>
-                                    <Button 
-                                        as="a" 
-                                        size="sm" 
-                                        fontSize="sm" 
-                                        colorScheme="purple" 
-                                        leftIcon={<Icon as={RiPencilLine} fontSize="16"/>}
-                                    >
-                                        Editar
-                                    </Button>
-                                </Td>
-                            </Tr>
-                        </Tbody>
-                    </Table>
-                    <Pagination />
-                </Box>
-            </Flex>
+  return (
+    <Box>
+      <Header />
+      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+        <Sidebar />
+        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+          <Flex mb="8" justify="space-between" align="center">
+            <Heading size="lg" fontWeight="normal">
+              {" "}
+              Usuários{" "}
+            </Heading>
+            <Link href="/users/create" passHref>
+              <Button
+                as="a"
+                size="sm"
+                fontSize="sm"
+                colorScheme="pink"
+                leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+              >
+                Criar novo
+              </Button>
+            </Link>
+          </Flex>
+
+          <Table colorScheme="whiteAlpha">
+            <Thead>
+              <Tr>
+                <Th px={["4", "4", "6"]} color="gray.300" width="8">
+                  <Checkbox colorScheme="pink" />
+                </Th>
+                <Th>Usuário</Th>
+                {isWideVersion && <Th>Data de cadastro</Th>}
+                <Th w="8"></Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr px="6">
+                <Td>
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Marianna Belniok</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      mariannabelniok@outlook.com
+                    </Text>
+                  </Box>
+                </Td>
+                {isWideVersion && <Td>04 de Abril, 2021</Td>}
+                {
+                  <Td>
+                    {isWideVersion && (
+                      <Button
+                        as="a"
+                        size="sm"
+                        fontSize="sm"
+                        colorScheme="purple"
+                        leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                      >
+                        Editar
+                      </Button>
+                    )}
+                  </Td>
+                }
+              </Tr>
+            </Tbody>
+          </Table>
+          <Pagination />
         </Box>
-    );
+      </Flex>
+    </Box>
+  );
 }
