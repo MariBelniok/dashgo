7cb2daabd316da6a6f8bd2bed9ab35ff93b28a1a
maribelniok <mariannabelniok@outlook.com>
2022-02-25 06:30:16 -0300

    repsonsive header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,17 +1,23 @@
 import { Avatar, Box, Flex,Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+    showProfileData: boolean;
+}
+
+export function Profile({ showProfileData = true}: ProfileProps) {
     return (
         <Flex align="center">
-        <Box mr="4" textAlign="right">
-            <Text>Marianna</Text>
-            <Text 
-                color="gray.300"
-                fontSize="small"
-            >
-                mariannabelniok@outlook.com
-            </Text>
-        </Box>
+        {showProfileData && (
+            <Box mr="4" textAlign="right">
+                <Text>Marianna</Text>
+                <Text 
+                    color="gray.300"
+                    fontSize="small"
+                >
+                    mariannabelniok@outlook.com
+                </Text>
+            </Box>
+        )}
         <Avatar size="md" name="Marianna Belniok"/>
     </Flex>
     );
